9c8b95800d97b6278e89d96767efae14d00f46be
asimuzzaman <mansib.raj@gmail.com>
2015-08-02 00:11:27 +0600

    Adding more database functions.
    ConnectDB(), query(), CloseDb().

--- includes/database.php
+++ includes/database.php
@@ -1,5 +1,21 @@
 <?php
   function ConnectDb() {
-    $connect = mysqli_connect("localhost","username","password","nsu-ed") or die ("Error connecting to database.");
+    global $connect;
+    $connect = mysqli_connect("localhost","root","1234","nsu-ed");
+
+    if(mysqli_connect_errno()) {
+      echo "Unable to connect to database: " . mysqli_connect_error() ."<br/>";
+    }
+  }
+
+  function query($query) {
+    global $connect;
+    $result = mysqli_query($connect,$query) or die ("Error in query.");
+    return $result;
+  }
+
+  function CloseDb() {
+    global $connect;
+    mysqli_close($connect);
   }
 ?>
