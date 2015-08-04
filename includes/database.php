54266ec3be0f13cf95c05e369246879a1c42a112
asimuzzaman <mansib.raj@gmail.com>
2015-08-04 22:31:57 +0600

    Recovering more files

---
+++ includes/database.php
@@ -0,0 +1,21 @@
+<?php
+  function ConnectDb() {
+    global $connect;
+    $connect = mysqli_connect("localhost","root","1234","nsu-ed");
+
+    if(mysqli_connect_errno()) {
+      echo "Unable to connect to database: " . mysqli_connect_error() ."<br/>";
+    }
+  }
+
+  function query($query) {
+    global $connect;
+    $result = mysqli_query($connect,$query) or die ("Error in query.");
+    return $result;
+  }
+
+  function CloseDb() {
+    global $connect;
+    mysqli_close($connect);
+  }
+?>
